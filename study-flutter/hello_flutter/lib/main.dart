962655b5040856edd5cc5fcbdb84ba2cac1b006c
Dong-Yong Lee <pfldy2850@gmail.com>
2023-02-23 20:24:24 +0900

    feat(study-flutter): add hello flutter app

---
+++ study-flutter/hello_flutter/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SplashScreen());
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            color: Colors.orange,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/icon.png',
+                    width: 200,
+                  ),
+                  const CircularProgressIndicator(),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
